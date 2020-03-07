1e83e787b97f5c36d671d2b5ea26978b2039d862
Natalman <natalnahm@gmail.com>
2020-03-07 16:23:12 -0600

    Added two containers with icons and text that are phone number and email. And then styled them a little bit.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
                 'Natalman Nahm',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
-                  fontSize: 40.0,
+                  fontSize: 35.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
@@ -35,6 +35,54 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                   color: Colors.teal.shade100,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+1 651 226 5240',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'natalnahm@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
