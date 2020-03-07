741a11d5c8faa822d76ba56ef24e73565dc64a8b
Natalman <natalnahm@gmail.com>
2020-03-06 20:43:32 -0600

    Added a column with 3 containers as its children, with a sizeBox.

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,32 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            width: 100.0,
-            height: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Text('Hello World!'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
